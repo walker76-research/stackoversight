c6447c6bf58561e2c7d6b1f93780956a66aeb0bc
Andrew Walker <Andrew_Walker2@baylor.edu>
2019-07-01 10:35:34 +0200

    Gets the metadata for the 20 most recent comments

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 import pprint
-from stackapi import StackAPI
+from stackapi import StackAPI, StackAPIError
 SITE = StackAPI('stackoverflow')
 comments = SITE.fetch('comments')
 
+post_ids = []
 for comment_metadata in comments["items"]:
-    pprint.pprint(comment_metadata)
+    # pprint.pprint(comment_metadata)
+    post_ids.append(comment_metadata['post_id'])
+
+post_ids = post_ids[:20]
+try:
+    posts = SITE.fetch('posts', ids=post_ids)
+    # pprint.pprint(post_text)
+except StackAPIError as e:
+    print(e.message)
+    exit(-1)
+
+for post_metadata in posts["items"]:
+    pprint.pprint(post_metadata)
