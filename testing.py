1d46573ece7f731b164e27707bc4e1642258f072
Andrew Walker <Andrew_Walker2@baylor.edu>
2019-07-02 09:57:00 +0200

    Searches for duplicates

--- testing.py
+++ testing.py
@@ -2,12 +2,18 @@ from io import StringIO
 from tokenize import generate_tokens
 import pprint
 from keyword_analyzer import KeywordAnalyzer
-from trie import TrieNode, add
+from trie import TrieNode, add, find_prefix
 
 keyword_analyzer = KeywordAnalyzer()
-code = "for i in range(1,11):\n" \
+code_base = "for i in range(1,11):\n" \
         "   n = 1\n" \
         "   for j in range(1, i+1):\n" \
+        "       n *= j\n" \
+        "   print('{}! = {}'.format(i, n))"
+
+code_sample = "for i in range(25,100):\n" \
+        "   n = 3\n" \
+        "   for j in range(2, i+5):\n" \
         "       n *= j\n" \
         "   print('{}! = {}'.format(i, n))"
 
@@ -31,23 +37,32 @@ PARAMETER
 INDENT
 '''
 
-tokens = []
-for token in generate_tokens(StringIO(code).readline):
-    pprint.pprint(token)
-    tokens.append((token[0], token[1]))
 
-start_token = "START"
-keywords = []
-for token in tokens:
+def get_tokens(code: str):
+    tokens = []
+    for token in generate_tokens(StringIO(code_base).readline):
+        # pprint.pprint(token)
+        tokens.append((token[0], token[1]))
 
-    if token[1] == '\n':
-        continue
+    start_token = "START"
+    keywords = []
+    for token in tokens:
 
-    if keyword_analyzer.is_keyword(token, start_token):
-        pprint.pprint(keyword_analyzer.get_keyword())
-        start_token = keyword_analyzer.get_keyword()
-        keywords.append(start_token)
+        if token[1] == '\n':
+            continue
+
+        if keyword_analyzer.is_keyword(token, start_token):
+            # pprint.pprint(keyword_analyzer.get_keyword())
+            start_token = keyword_analyzer.get_keyword()
+            keywords.append(start_token)
+
+    return tokens
+
+
+base_tokens = get_tokens(code_base)
+testing_tokens = get_tokens(code_sample)
 
 root = TrieNode('*')
-add(root, keywords)
-print(root)
+add(root, base_tokens)
+
+print(find_prefix(root, testing_tokens))
