93de1e27aa3d6c33e340316a3d3410b20d38651a
Jonathan Simmons <jdsimmons1@mail.umhb.edu>
2019-07-02 11:01:15 +0200

    Naïve approach on seeing if code is valid Python. Just checks if it can compile to Python or not.
    Test via astfformer_object.status == ASTFStatus.SUCCESS

--- astformer.py
+++ astformer.py
@@ -1,22 +1,46 @@
 import ast
 from pprint import pprint
+from enum import Enum
+
+DEBUG = True
+
+class ASTFormerStatus(Enum):
+    SUCCESS = 1
+    FAILURE = 0
 
 class ASTFormer:
     # Create an object of ASTFormer with the Python source code
     # to create an abstract syntax tree of the code. form_ast()
     # iterates the tree.
+    # Sets status to SUCCESS if valid Python code (determined by if AST can be formed).
     def __init__(self, source):
         self.source = source
-        self.tree = ast.parse(str(source))
         self.stats = {"import": [], "from": []}
         self.code = source
+        try:
+            self.tree = ast.parse(str(source))
+            self.status = ASTFormerStatus.SUCCESS
+        except SyntaxError:
+            self.tree = None
+            self.status = ASTFormerStatus.FAILURE
+            if DEBUG:
+                pprint("Failed to create syntax tree. Cannot compile the code to Python.")
+                pprint(SyntaxError)
+        except:
+            self.tree = None
+            self.status = ASTFormerStatus.FAILURE
+            if DEBUG: pprint("Unknown error")
 
     def form_ast(self):
-        analyzer = ASTIterator()
-        analyzer.visit(self.tree)
+        if self.status == ASTFormerStatus.SUCCESS:
+            analyzer = ASTIterator()
+            analyzer.visit(self.tree)
 
     def dump(self):
-        return ast.dump(self.tree)
+        if self.status == ASTFormerStatus.SUCCESS:
+            return ast.dump(self.tree)
+        else:
+            return None
 
     def dump_tofile(self):
         out = open("dump_file.txt", "w")
