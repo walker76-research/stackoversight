b7e3b7586ff7bd1114a786bfd5c40898b3684351
Jonathan Simmons <jdsimmons1@mail.umhb.edu>
2019-07-02 11:47:52 +0200

    Updated picklizer

--- gitpickler.py
+++ gitpickler.py
@@ -1,11 +1,24 @@
 import pickle
 
 # Separate class in case we want to handle serialization further
+# Default file name: "gitdump.pickle"
 class Picklizer:
-    def __init__(self, data):
-        with open("gitdump.pickle","wb") as handle:
+    def __init__(self, data=None, file="gitdump.pickle"):
+        if data != None:
+            with open(file,"wb") as handle:
+                pickle.dump(data, handle, protocol=pickle.HIGHEST_PROTOCOL)
+            self.data = data
+
+    def save(self, data, file="gitdump.pickle"):
+        with open(file, "wb") as handle:
             pickle.dump(data, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
-    def open(self):
-        with open("gitdump.pickle", "rb") as handle:
-            return pickle.load(handle)
+    def open(self, file="gitdump.pickle"):
+        with open(file, "rb") as handle:
+            self.data = pickle.load(handle)
+        return self.data
+
+
+def test_picklizer():
+    pick = Picklizer()
+    print(pick.open())
