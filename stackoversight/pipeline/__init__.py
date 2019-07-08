6b1c49181b75a08fa816a50ac456bab1f2f63180
Andrew Walker <Andrew_Walker2@baylor.edu>
2019-07-08 11:04:50 +0200

    Renames processing step

--- stackoversight/pipeline/__init__.py
+++ stackoversight/pipeline/__init__.py
@@ -1,4 +1,4 @@
 from stackoversight.pipeline.pipeline import Pipeline
-from stackoversight.pipeline.processingstep import ProcessingStep
+from stackoversight.pipeline.processing_step import ProcessingStep
 from stackoversight.pipeline.tokenizer import Tokenizer
 from stackoversight.pipeline.keyword_extractor import KeywordExtractor
