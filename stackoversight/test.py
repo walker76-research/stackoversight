e9255c687662c16469d9b8f3fb5df4cf9a180eb8
Jonathan Simmons <john_simmons2@baylor.edu>
2019-07-19 13:48:03 +0200

    Updated test script to have a mini demo of the pipeline at its current state

--- stackoversight/test.py
+++ stackoversight/test.py
@@ -1,8 +1,6 @@
 from pprint import pprint
-from stackoversight.keyword_analyzer import KeywordAnalyzer
-from stackoversight.pipeline import Pipeline, KeywordExtractor, Tokenizer
+from pipeline import Pipeline, KeywordExtractor, Tokenizer, Sanitizer, Filter
 
-keyword_analyzer = KeywordAnalyzer()
 code_base = "for i in range(1,11):\n" \
         "   n = 1\n" \
         "   for j in range(1, i+1):\n" \
@@ -11,9 +9,16 @@ code_base = "for i in range(1,11):\n" \
 
 code_sample = "for i in range(25,100):\n" \
         "   n = 3\n" \
+        "   # This is a comment!\n" \
+        "   # Notice how it is found as a 'NONE'!\n" \
         "   for j in range(2, i+5):\n" \
         "       n *= j\n" \
+        "       # Another comment!\n" \
         "   print('{}! = {}'.format(i, n))"
+
+not_code =  "This is an example of\nsomething that is not even a code snippet!\n" \
+            " it contains code such as: for i in range(1, 10):\n" \
+            " But it would never compile."
 
 '''
 KEYWORD_FOR
@@ -36,12 +41,18 @@ INDENT
 '''
 
 processing_steps = [
+    Sanitizer(),
+    Filter(),
     Tokenizer(),
     KeywordExtractor()
 ]
 pipeline = Pipeline()
 pipeline.set_steps(processing_steps)
-keywords = pipeline.feed([code_base, code_sample])
+keywords = pipeline.feed([code_base, code_sample, not_code])
 
-for keyword_arr in keywords:
-    pprint(keyword_arr)
+print("========THE OUTPUT========")
+print(keywords[0])
+print("-----")
+print(keywords[1])
+print("-----")
+print(keywords[2])
